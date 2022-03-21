17fee77d0d78823c850986377cf6ff9a9ebc2751
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-03-21 23:01:26 +0500

    Task 5 done.

--- Assignment 1/Task_6/main.c
+++ Assignment 1/Task_6/main.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    if(fork() == 0) {
-        printf("A Child Created\n");
+int main()
+{
+    if (fork() == 0)
+    {
+        printf("Child Created, PID: %d\n", getpid());
     }
-    if(fork() == 0) {
-        printf("A Child Created\n");
+    if (fork() == 0)
+    {
+        printf("Child Created, PID: %d\n", getpid());
     }
-    if(fork() == 0) {
-        printf("A Child Created\n");
+    if (fork() == 0)
+    {
+        printf("Child Created, PID: %d\n", getpid());
     }
-    if(fork() == 0) {
-        printf("A Child Created\n");
+    if (fork() == 0)
+    {
+        printf("Child Created, PID: %d\n", getpid());
     }
 
     return 0;
