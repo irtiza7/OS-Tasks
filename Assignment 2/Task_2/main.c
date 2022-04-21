554908062d17466f3ce0d23dfd4802e4426a3a6d
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-04-21 21:51:20 +0500

    Changed Task 2's implementation method and updated report.

--- Assignment 2/Task_2/main.c
+++ Assignment 2/Task_2/main.c
@@ -3,22 +3,36 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void *print()
+typedef struct Information
 {
-	printf("Process ID: %d and Thread ID: %lu\n", getpid(), pthread_self());
+	pid_t pid;
+	pthread_t tid;
+} INFORMATION;
+
+void *threadHandler()
+{
+	INFORMATION *info = malloc(sizeof(INFORMATION));
+	info->pid = getpid();
+	info->tid = pthread_self();
+	pthread_exit(info);
 }
 
 int main()
 {
 	pthread_t threads[7];
+	INFORMATION *returnedValues[7];
 
 	for (int i = 0; i < 7; i++)
 	{
-		pthread_create(&threads[i], NULL, print, NULL);
+		pthread_create(&threads[i], NULL, threadHandler, NULL);
 	}
 	for (int j = 0; j < 7; j++)
 	{
-		pthread_join(threads[j], NULL);
+		pthread_join(threads[j], (INFORMATION *)&returnedValues[j]);
+	}
+	for (int k = 0; k < 7; k++)
+	{
+		printf("Process ID: %d and Thread ID: %lu\n", returnedValues[k]->pid, returnedValues[k]->tid);
 	}
 
 	return 0;
