83ea249ae68ad4c5fa8bf917ae8a79c75ea113cf
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-06-05 13:37:09 +0500

    Practiced process barrier operation.

---
+++ Message Passing Interface (MPI) Programming/barrier.c
@@ -0,0 +1,28 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int my_id;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+
+    int num_procs;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    if(my_id == 0) {
+        printf("This is process: %d\n", my_id);
+    } else if(my_id == 1) {
+        printf("This is process: %d\n", my_id);
+        sleep(5);
+    } else if(my_id == 2) {
+        printf("This is process: %d\n", my_id);
+        sleep(10);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("This is process: %d, and i have crossed barrier.\n", my_id);
+
+    MPI_Finalize();
+}
