6363ef660a5d96f424a8eb068eb803ac4af665e5
Muhammad Irtiza <m7.irtiza@gmail.com>
2022-09-16 23:35:02 +0500

    Q2 of Task 3 done.

--- Lab_Tasks/Task_3/Q2/main.c
+++ Lab_Tasks/Task_3/Q2/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define SIZE 10
 
 void printArray(int array[]);
-void sortAndPrintArray(int array[]);
+void ascSortAndPrintArray(int array[]);
+void desSortAndPrintArray(int array[]);
 
 int main(int argc, char *argv[])
 {
@@ -14,13 +17,25 @@ int main(int argc, char *argv[])
     {
         numbers[i] = atoi(argv[i + 1]);
     }
-    pid_t child_pid;
-    child_pid = fork();
 
-    if (child_pid == 0)
+    if (fork() == 0)
     {
-        sortAndPrintArray(numbers);
-        printf("\nThis is Child Process with PID: %d \n", getpid());
+        printf("This is Child Process with PID: %d and Parent ID: %d\n", getpid(), getppid());
+        ascSortAndPrintArray(numbers);
+    }
+    else
+    {
+        wait(NULL);
+        if (fork() == 0)
+        {
+            printf("\nThis is Child Process with PID: %d and Parent ID: %d\n", getpid(), getppid());
+            desSortAndPrintArray(numbers);
+        }
+        else
+        {
+            wait(NULL);
+            printf("\nI am Parent with ID: %d\n", getpid());
+        }
     }
     return 0;
 }
@@ -32,13 +47,31 @@ void printArray(int array[])
         printf("%d ", array[i]);
     }
 }
-void sortAndPrintArray(int array[])
+
+void ascSortAndPrintArray(int array[])
 {
     for (int i = 0; i < SIZE; i++)
     {
         for (int j = i + 1; j < SIZE; j++)
         {
             if (array[i] > array[j])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    printArray(array);
+}
+
+void desSortAndPrintArray(int array[])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = i + 1; j < SIZE; j++)
+        {
+            if (array[i] < array[j])
             {
                 int temp = array[i];
                 array[i] = array[j];
